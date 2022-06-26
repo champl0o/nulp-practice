5d9988c86106a053ba07802536a8299178c1703b
champl0o <podilkaroman@gmail.com>
2022-06-26 17:08:25 +0300

    Update csp

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -8,14 +8,14 @@
 
 Rails.application.configure do
   config.content_security_policy do |policy|
-    # policy.default_src :self, :https
-    # policy.font_src    :self, :https, :data
-    # policy.img_src     :self, :https, :data
-    # policy.object_src  :none
-    policy.script_src :self, :https, :unsafe_inline
-    # policy.style_src   :self, :https
+    policy.default_src :self, :https
+    policy.font_src    :self, :https, :data
+    policy.img_src     :self, :https, :data
+    policy.object_src  :none
+    policy.script_src  :self, :https, :unsafe_inline
+    policy.style_src   :self, :https
     # Specify URI for violation reports
-    # policy.report_uri "/csp-violation-report-endpoint"
+    policy.report_uri '/csp-violation-report-endpoint'
   end
 
   # Generate session nonces for permitted importmap and inline scripts
