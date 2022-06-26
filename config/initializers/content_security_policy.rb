93bebcc53f56c911464a74e23d1f695f14ddb8df
champl0o <podilkaroman@gmail.com>
2022-06-26 17:19:16 +0300

    Update csp

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
     policy.font_src    :self, :https, :data
     policy.img_src     :self, :https, :data
     policy.object_src  :none
-    policy.script_src  :https, :unsafe_inline
+    policy.script_src  :self, :https, :unsafe_inline, 'https://js.stripe.com', 'https://www.paypal.com'
     policy.style_src   :self, :https
     # Specify URI for violation reports
     policy.report_uri '/csp-violation-report-endpoint'
